b2299d3b2a21419de7f97322aafcac08cc6271a8
The_JT <67649495+JT-ceh@users.noreply.github.com>
2023-03-02 11:05:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,92 @@
 const express=require('express');
+var {MongoClient} = require('mongodb');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const {greet,els,good,help,rSad,join,sing,come,dance,wont,didGood,love,gn,morning,miss,marry}=require('./mind/da.json');
-
-const moods=[greet,good,help,rSad,join,sing,come,dance,wont,didGood,love,gn,morning,miss,marry];
 const app=express();
+let db
+
 app.use(cors());
 app.use(bodyParser.json());
 
-const start=async function(){await app.listen(3005,()=>console.log("Listening on 3005"));}()
+async function start(){
+  const client = new MongoClient("mongodb+srv://vee:vee@1306@veela.ws8emjn.mongodb.net/?retryWrites=true&w=majority");
+  await client.connect();
+  db=client.db("thevalley");
+  app.listen(3001,()=>console.log("Listening on 3001"));
+}
+start()
 
 //testing pinging server
-app.get('/',(req,res)=>{res.send("<b>Hello there, i'm up and running.</b>")})
+app.get('/',(req,res)=>{res.send("<b>Hello there minds, i'm up and running.</b>")})
 
-app.post('/veela',function(req,res){
-  const stop=["you","is","of","are","i","so","the","them","me"];const d=/\d/g;const char=/\W/g;let result=[];let found=[];let notfound=[];let values=req.body.value.split(' ');
-    function log(x){console.log(x)};let rp=[],sug=[]// for console debugging
-    values=values.filter(x=>!stop.includes(x));values=values.filter(xz=>!d.test(xz));values=values.filter(xy=>!char.test(xy));// remove stop words and numbers and characters
-    values=[...new Set(values)];// remove dublicates
-    values.map(x=>log(x));
-    moods.forEach((mood,k)=>{
-      console.log(mood)
-      values.forEach((x)=>{
-       // let x1=x.split('');x1=x1.filter(xa=>!char.test(xa));x1=x1.filter(xb=>!d.test(xb));x1=x1.join('')//remove numbers and special characters from value
-       const re=RegExp(mood.keys1,'i');
-       if(x.search(re)!=-1&&re.test(x)&&re.test(x)){result=[...result,1];found=[...found,x]}// if found
-        else{result=[...result,0];notfound=[...notfound,x]}// if not found
-      });// loop through user input values
-      decide(mood,req.body.value);log(found);result=[];found=[];notfound=[];
-    });// loop through moods 
-  
-    function decide(m,value){ // fx to determine and predict the closest response
-      const fsize=result.length;result=result.filter(x=>x);const clen=result.length;let tes=false;// get full size=>remove 0(not found)=>get remaining length
-      if(clen>=0.5*fsize){tes=true;rp.splice(0,0,m.rp[Math.floor(Math.random()*m.rp.length)]);sug.splice(0,0,m.sug)}// if current size is more than fullsize/2 then correct
-      else{
-          if(clen>0.1*fsize){tes=true;rp.splice(0,0,m.rp[Math.floor(Math.random()*m.rp.length)]);sug.splice(0,0,m.sug)}//if at least one size is more than fullsize/2 then correct
-          else{!tes&&rp.push(els.rp[Math.floor(Math.random()*els.rp.length)]);!tes&&sug.push(els.sug)}
-        }
-    }
-    rp=[...new Set(rp)];sug=[...new Set(sug)];sug=sug.flat();
-    console.log("Rps "+rp+"\nSug: "+sug);
-    res.send({rps:rp,sugs:sug});
+//post attendance user
+app.post('/preg',function (req,res){
+  const tcin=req.body.cin;
+  if(!tcin){db.collection('users').updateOne({name:req.body.name},{$set:{dtime:req.body.utime,cout:req.body.cout}},function(err,res){if(err)throw err;console.log("1 updated well")})}
+  else{
+    const myReg = {name:req.body.name,dept:req.body.dep,utime:req.body.utime,dtime:null,cin:req.body.cin,cout:req.body.cout};
+    db.collection('users').insertOne(myReg, function(err, res) {if(err)throw err;console.log("1 document inserted in attandance");});}
+})
+
+//post concern from whisleblower
+app.post('/pmobile',function (req,res){
+  const myOb = {description: req.body.description, summary:req.body.summary,sconc:req.body.conc, eemail:req.body.emaill};
+  db.collection("mobile").insertOne(myOb, function(err, res) {if(err)throw err;console.log("1 document inserted Cons");});
+})
+
+//post join user
+app.post('/pjoin',function (req,res){
+  const myOb = {description: req.body.description, summary:req.body.summary,sconc:req.body.conc, eemail:req.body.emaill};
+  db.collection("minds").insertOne(myOb, function(err, res) {if(err)throw err;console.log("1 document inserted Cons");});
+})
+
+//post concern from whisleblower
+app.post('/pai',function (req,res){
+  const myOb = {name:req.body.name,dep:req.body.dep};
+  console.log("Testing "+myOb.name+"and you are"+myOb.dep);
+  db.collection("ai").insertOne(myOb, function(err, res) {if(err)throw err;console.log("1 document inserted in Sug");});
+})
+
+//post concern from whisleblower
+app.post('/pweb',function (req,res){
+  const web={name:req.body.name,addr:req.body.dep,minds:req.body.minds,minds1:req.body.minds1,minds2:req.body.minds2};
+  db.collection("web").insertOne(web, function(err, res) {if(err)throw err;console.log("1 document inserted in trucks");});
+})
+
+//post concern from whisleblower
+app.post('/pdesktop',function (req,res){
+  const myTruck={name:req.body.name,addr:req.body.dep,size:req.body.utime,cin:req.body.cin,cout:req.body.cout};
+  db.collection("desktop").insertOne(myTruck, function(err, res) {if(err)throw err;console.log("1 document inserted in trucks");});
+})
+
+//user querries
+//get user
+app.post('/ulogin',function(req,res){
+  //db.collection("concerns").insertOne({name:"talent",lpas:"myself"}, function(err, res){if(err)throw err;console.log("1 document inserted");});
+  const user={name: req.body.name, lpas:req.body.lpas};
+  db.collection('users').find({name:user.name,lpas:user.lpas}).toArray(function(err,result){if(err)throw err;res.send({data:result?true:false});})
+})
+
+//get concerns from whisleblower
+app.get('/gweb',function(req,res){
+  db.collection("web").find({}).toArray(function(err, result){if(err)throw err;res.send(result);});
+})
+//get concerns from whisleblower
+app.get('/gmobile',function(req,res){
+  db.collection("mobile").find({}).toArray(function(err, result){if(err)throw err;res.send(result);});
+})
+
+//get attendance register users
+app.get('/gai', function (req, res) {
+  db.collection('ai').find({}).toArray(function (err, result) { if (err) throw err; res.send(result); })
+})
+
+//get attendance register users
+app.get('/gdesktop', function (req, res) {
+  db.collection('desktop').find({}).toArray(function (err, result) { if (err) throw err; res.send(result); })
+})
+
+//get joins
+app.get('/gjoin', function (req, res) {
+  db.collection('desktop').find({}).toArray(function (err, result) { if (err) throw err; res.send(result); })
 })
